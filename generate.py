fc1ca1e4029727c2bd765e6441a2cba84f69691b
Daniel Micay <danielmicay@gmail.com>
2022-01-18 11:27:23 -0500

    support release channels in metadata generation

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ import shutil
 import subprocess
 from pathlib import Path
 
-top = "extracted-apps"
+channels = "stable", "beta"
 
 shutil.rmtree("apps", ignore_errors=True)
 os.mkdir("apps")
@@ -17,50 +17,53 @@ shutil.copy("apps.0.pub", "apps")
 
 apps = {}
 
-for app_id in os.listdir(top):
-    metadata = {"label": "", "versionCode": -1, "dependencies": [], "packages": [], "hashes": []}
-    apps[app_id] = {"stable": metadata}
+for channel in channels:
+    top = "apps-" + channel
+    for app_id in os.listdir(top):
+        metadata = {"label": "", "versionCode": -1, "dependencies": [], "packages": [], "hashes": []}
 
-    src_dir = os.path.join(top, app_id)
-    src_packages = os.listdir(src_dir)
-    if len(src_packages) == 1:
-        base_apk = src_packages[0]
-    else:
-        base_apk = "base.apk"
+        src_dir = os.path.join(top, app_id)
+        src_packages = os.listdir(src_dir)
+        if len(src_packages) == 1:
+            base_apk = src_packages[0]
+        else:
+            base_apk = "base.apk"
 
-    badging = subprocess.check_output(["aapt", "dump", "badging", os.path.join(src_dir, base_apk)])
-    lines = badging.split(b"\n")
+        badging = subprocess.check_output(["aapt", "dump", "badging", os.path.join(src_dir, base_apk)])
+        lines = badging.split(b"\n")
 
-    for kv in shlex.split(lines[0].decode()):
-        if kv.startswith("versionCode"):
-            version_code = int(kv.split("=")[1])
-            metadata["versionCode"] = version_code
+        for kv in shlex.split(lines[0].decode()):
+            if kv.startswith("versionCode"):
+                version_code = int(kv.split("=")[1])
+                metadata["versionCode"] = version_code
 
-    for line in lines[1:-1]:
-        kv = shlex.split(line.decode())
-        if kv[0].startswith("application-label:"):
-            metadata["label"] = kv[0].split(":")[1]
-        elif kv[0].startswith("uses-static-library:"):
-            metadata["dependencies"].append(kv[1].split("=")[1])
+        for line in lines[1:-1]:
+            kv = shlex.split(line.decode())
+            if kv[0].startswith("application-label:"):
+                metadata["label"] = kv[0].split(":")[1]
+            elif kv[0].startswith("uses-static-library:"):
+                metadata["dependencies"].append(kv[1].split("=")[1])
 
-    if app_id == "com.google.android.gms":
-        metadata["dependencies"] = ["com.google.android.gsf"]
-    elif app_id == "com.android.vending":
-        metadata["dependencies"] = ["com.google.android.gsf", "com.google.android.gms"]
+        if app_id == "com.google.android.gms":
+            metadata["dependencies"] = ["com.google.android.gsf"]
+        elif app_id == "com.android.vending":
+            metadata["dependencies"] = ["com.google.android.gsf", "com.google.android.gms"]
 
-    app_dir = os.path.join("apps", "packages", app_id, str(version_code))
-    if len(src_packages) == 1:
-        os.makedirs(app_dir)
-        shutil.copyfile(os.path.join(src_dir, base_apk), os.path.join(app_dir, "base.apk"))
-    else:
-        shutil.copytree(src_dir, app_dir)
+        app_dir = os.path.join("apps", "packages", app_id, str(version_code))
+        if len(src_packages) == 1:
+            os.makedirs(app_dir)
+            shutil.copyfile(os.path.join(src_dir, base_apk), os.path.join(app_dir, "base.apk"))
+        else:
+            shutil.copytree(src_dir, app_dir)
 
-    for package in os.listdir(app_dir):
-        h = hashlib.new("sha256")
-        with open(os.path.join(app_dir, package), "rb") as f:
-            h.update(f.read())
-        metadata["hashes"].append(h.hexdigest())
-        metadata["packages"].append(package)
+        for package in os.listdir(app_dir):
+            h = hashlib.new("sha256")
+            with open(os.path.join(app_dir, package), "rb") as f:
+                h.update(f.read())
+            metadata["hashes"].append(h.hexdigest())
+            metadata["packages"].append(package)
+
+        apps.setdefault(app_id, {})[channel] = metadata
 
 metadata = {
     "time": int(datetime.datetime.utcnow().timestamp()),
